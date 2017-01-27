1db4dfbf04a6449131f201cdbbc92347f9c0ef3f
caseycallow <casey.callow@productops.com>
2017-01-26 18:47:46 -0800

    implementing Mustache Gulp task and updated Watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,42 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var bs = require('browser-sync').create();
-var mustache = require("gulp-mustache");
+var mustache = require('gulp-mustache');
+var htmlbeautify = require('gulp-html-beautify');
+var fs  = require('fs');
 
-// browser-sync
+// Run Browsersync
 gulp.task('browser-sync', ['sass'], function() {
   bs.init({
-    server: "./",
+    server: './',
     port: 8080
   });
 });
 
-//Compile Sass
+//Compile SASS
 gulp.task('sass', function () {
   gulp.src('src/styles/scss/*.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('src/styles/css/'))
-  .pipe(bs.reload({stream: true}));
+    .pipe(sass())
+    .pipe(gulp.dest('src/styles/css/'))
+    .pipe(bs.reload({stream: true}));
 });
 
 //Compile Mustache
-// gulp.task('mustache', function () {
-//   gulp.src("src/templates/*.mustache")
-//     .pipe(mustache({
-//       msg: "Compiling Mustache templates"
-//     }))
-//     //need gulp-rename to change mustache to html
-//     //watch needs to look at mustache files
-//     .pipe(gulp.dest("./dist"));
-// });
+gulp.task('mustache', function() {
+  var content = fs.readFileSync('./src/data/data.json');
+  var components = JSON.parse(content);
+  gulp.src('./src/components/index.mustache')
+    .pipe(mustache(components, {extension: '.html'}))
+    .pipe(htmlbeautify({indent_with_tabs: true}))
+    .pipe(gulp.dest('./'));
+})
 
 //Watch Files
 gulp.task('watch', ['browser-sync'], function () {
-  gulp.watch("src/styles/scss/**/*.scss", ['sass']);
-  gulp.watch("*.html").on('change', bs.reload);
+  gulp.watch('src/styles/scss/**/*.scss', ['sass']);
+  gulp.watch('src/components/*.mustache', ['mustache']).on('change', bs.reload);
+  gulp.watch('src/data/*.json', ['mustache']).on('change', bs.reload);
 });
 
-gulp.task('default',['watch', 'sass', 'browser-sync']);
+//Set up default task
+gulp.task('default',['watch', 'mustache', 'sass', 'browser-sync']);
