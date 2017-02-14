052d67fdd48df5aade4538fcb92da6085d9c7faf
caseycallow <casey.callow@productops.com>
2017-02-13 21:19:30 -0800

    config proj to generate multiple pages

--- gulpfile.js
+++ gulpfile.js
@@ -25,16 +25,17 @@ gulp.task('sass', function () {
 gulp.task('mustache', function() {
   var content = fs.readFileSync('./src/data/data.json');
   var data = JSON.parse(content);
-  gulp.src('./src/templates/index.mustache')
+  var pages = './src/pages/*.mustache'
+  gulp.src(pages)
     .pipe(mustache(data, {extension: '.html'}))
     .pipe(htmlbeautify({indent_with_tabs: true}))
     .pipe(gulp.dest('./'));
-})
+});
 
 //Watch Files
 gulp.task('watch', ['browser-sync'], function () {
   gulp.watch('src/styles/scss/**/*.scss', ['sass']);
-  gulp.watch('src/templates/*.mustache', ['mustache']).on('change', bs.reload);
+  gulp.watch('src/**/*.mustache', ['mustache']).on('change', bs.reload);
   gulp.watch('src/data/*.json', ['mustache']).on('change', bs.reload);
   gulp.watch('src/scripts/*.js').on('change', bs.reload);
 });
