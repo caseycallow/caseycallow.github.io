d1ed262ae8cea18d5892d7e3d12a64ece9ea5f65
caseycallow <casey.callow@productops.com>
2017-02-20 00:39:31 -0800

    simplify JSON/Mustache workflow

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('mustache', function() {
 });
 
 //Watch Files
-gulp.task('watch', ['browser-sync'], function () {
+gulp.task('watch', function () {
   gulp.watch('src/styles/scss/**/*.scss', ['sass']);
   gulp.watch('src/**/*.mustache', ['mustache']).on('change', bs.reload);
   gulp.watch('src/data/*.json', ['mustache']).on('change', bs.reload);
