4e5583bb758e12ae47ce284b665cd1df5a27b143
caseycallow <casey@udacity.com>
2019-02-21 12:15:49 -0800

    update title

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,64 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var bs = require('browser-sync');
-var mustache = require('gulp-mustache');
-var htmlmin = require('gulp-htmlmin');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var fs  = require('fs');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var bs = require("browser-sync");
+var mustache = require("gulp-mustache");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var fs = require("fs");
 
 // Run Browsersync
-gulp.task('browser-sync', function() {
+gulp.task("browser-sync", function() {
   bs.init({
-    server: './dist',
+    server: "./dist",
     port: 8080,
     notify: false
   });
 });
 
 //Compile and minify Sass
-gulp.task('sass', function() {
-  gulp.src('src/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+gulp.task("sass", function() {
+  gulp
+    .src("src/styles/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(autoprefixer())
-    .pipe(rename('styles.min.css'))
-    .pipe(gulp.dest('./dist'))
-    .pipe(bs.reload({stream: true}));
+    .pipe(rename("styles.min.css"))
+    .pipe(gulp.dest("./dist"))
+    .pipe(bs.reload({ stream: true }));
 });
 
 //Compile Mustache
-gulp.task('mustache', function() {
-  var content = fs.readFileSync('src/data/data.json');
+gulp.task("mustache", function() {
+  var content = fs.readFileSync("src/data/data.json");
   var data = JSON.parse(content);
-  var pages = 'src/templates/{index,portfolio}.mustache'
-  gulp.src(pages)
-    .pipe(mustache(data, {extension: '.html'}))
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('./dist'));
+  var pages = "src/templates/{index,portfolio}.mustache";
+  gulp
+    .src(pages)
+    .pipe(mustache(data, { extension: ".html" }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("./dist"));
 });
 
 //Minify JS
-gulp.task('scripts', function() {
-  return gulp.src('src/scripts/*.js')
-    .pipe(rename('scripts.min.js'))
+gulp.task("scripts", function() {
+  return gulp
+    .src("src/scripts/*.js")
+    .pipe(rename("scripts.min.js"))
     .pipe(uglify())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest("./dist"));
 });
 
 //Watch Files
-gulp.task('watch', function() {
-  gulp.watch('src/styles/**/*.scss', ['sass']);
-  gulp.watch('src/**/*.mustache', ['mustache']).on('change', bs.reload);
-  gulp.watch('src/data/*.json', ['mustache']).on('change', bs.reload);
-  gulp.watch('src/scripts/*.js', ['scripts']).on('change', bs.reload);
+gulp.task("watch", function() {
+  gulp.watch("src/styles/**/*.scss", ["sass"]);
+  gulp.watch("src/**/*.mustache", ["mustache"]).on("change", bs.reload);
+  gulp.watch("src/data/*.json", ["mustache"]).on("change", bs.reload);
+  gulp.watch("src/scripts/*.js", ["scripts"]).on("change", bs.reload);
 });
 
+//Build Files
+gulp.task("build", ["mustache", "sass", "scripts"]);
+
 //Set up default task
-gulp.task('default',['watch', 'mustache', 'sass', 'scripts', 'browser-sync']);
+gulp.task("default", ["watch", "mustache", "sass", "scripts", "browser-sync"]);
