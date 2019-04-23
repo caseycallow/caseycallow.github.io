3129f2d0e630fb93f68c47b9299cb58d933a90f1
caseycallow <casey@udacity.com>
2019-04-23 16:04:16 -0700

    update bio

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,58 +1,85 @@
 //Update iPhone clock and copyright year
-var tmonth = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+var tmonth = new Array(
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+);
 
 function GetClock() {
   var d = new Date();
-  var nmonth = d.getMonth(), ndate = d.getDate(), nyear = d.getYear();
+  var nmonth = d.getMonth(),
+    ndate = d.getDate(),
+    nyear = d.getYear();
   if (nyear < 1000) nyear += 1900;
-  var nhour = d.getHours(), nmin = d.getMinutes(), ap;
+  var nhour = d.getHours(),
+    nmin = d.getMinutes(),
+    ap;
   if (nhour == 0) {
-    ap =' AM';
+    ap = " AM";
     nhour = 12;
-  }
-  else if (nhour < 12) {
-    ap = ' AM';
-  }
-  else if (nhour == 12) {
-    ap = ' PM';
-  }
-  else if (nhour > 12) {
-    ap = ' PM';
+  } else if (nhour < 12) {
+    ap = " AM";
+  } else if (nhour == 12) {
+    ap = " PM";
+  } else if (nhour > 12) {
+    ap = " PM";
     nhour -= 12;
   }
-  if (nmin <= 9) nmin = '0' + nmin;
+  if (nmin <= 9) nmin = "0" + nmin;
 
   // update iPhone clock
-  document.getElementById('clock').innerHTML = '' + nhour + ':' + nmin + ap + '';
+  // document.getElementById('clock').innerHTML = '' + nhour + ':' + nmin + ap + '';
 
   // update copyright year
-  document.getElementById('copyright-year').innerHTML = nyear;
+  document.getElementById("copyright-year").innerHTML = nyear;
 }
-window.onload = function(){
+window.onload = function() {
   GetClock();
   setInterval(GetClock, 1000);
-}
+};
 
 // expand teaser when clicked
-$('.teaser').click(function(){
-  $(this).next('.project-detail').addClass('is-open');
+$(".teaser").click(function() {
+  $(this)
+    .next(".project-detail")
+    .addClass("is-open");
 });
 
 // collapse teaser when close icon clicked, hide video prototype
-$('.icon-close').click(function(e){
+$(".icon-close").click(function(e) {
   e.stopPropagation();
-  $(this).parents('.project-detail').removeClass('is-open');
-  $(this).parents('.main').find('.proto-vid').removeClass('is-playing');
+  $(this)
+    .parents(".project-detail")
+    .removeClass("is-open");
+  $(this)
+    .parents(".main")
+    .find(".proto-vid")
+    .removeClass("is-playing");
 });
 
 // open prototype video when open case study
-$('.teaser--prototype').click(function(){
-  $(this).parents('.main').find('.proto-vid').addClass('is-playing');
+$(".teaser--prototype").click(function() {
+  $(this)
+    .parents(".main")
+    .find(".proto-vid")
+    .addClass("is-playing");
 });
 
 //Scroll to top -- doesnt work on mobile
-$('.js-scroll-top').click(function(){
-  $('.project-detail').animate({
-    scrollTop: 0
-  }, 'fast');
+$(".js-scroll-top").click(function() {
+  $(".project-detail").animate(
+    {
+      scrollTop: 0
+    },
+    "fast"
+  );
 });
