73655b45f4e1e535a5834ffd16da0a794d0a9516
caseycallow <casey.callow@gmail.com>
2019-07-24 22:04:24 -0700

    update to gulp 4 + bump deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,63 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var autoprefixer = require("gulp-autoprefixer");
-var bs = require("browser-sync");
-var mustache = require("gulp-mustache");
-var htmlmin = require("gulp-htmlmin");
-var uglify = require("gulp-uglify");
-var rename = require("gulp-rename");
-var fs = require("fs");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const bs = require("browser-sync");
+const mustache = require("gulp-mustache");
+const htmlmin = require("gulp-htmlmin");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const fs = require("fs");
 
-// Run Browsersync
-gulp.task("browser-sync", function() {
-  bs.init({
-    server: "./dist",
-    port: 8080,
-    notify: false
-  });
-});
-
-//Compile and minify Sass
-gulp.task("sass", function() {
-  gulp
+function styles() {
+  return gulp
     .src("src/styles/*.scss")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(autoprefixer())
     .pipe(rename("styles.min.css"))
     .pipe(gulp.dest("./dist"))
     .pipe(bs.reload({ stream: true }));
-});
+};
 
-//Compile Mustache
-gulp.task("mustache", function() {
+function templates() {
   var content = fs.readFileSync("src/data/data.json");
   var data = JSON.parse(content);
   var pages = "src/templates/{index,portfolio}.mustache";
-  gulp
+  return gulp
     .src(pages)
     .pipe(mustache(data, { extension: ".html" }))
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("./dist"));
-});
+};
 
-//Minify JS
-gulp.task("scripts", function() {
+function scripts() {
   return gulp
     .src("src/scripts/*.js")
     .pipe(rename("scripts.min.js"))
     .pipe(uglify())
     .pipe(gulp.dest("./dist"));
-});
+};
 
-//Watch Files
-gulp.task("watch", function() {
-  gulp.watch("src/styles/**/*.scss", ["sass"]);
-  gulp.watch("src/**/*.mustache", ["mustache"]).on("change", bs.reload);
-  gulp.watch("src/data/*.json", ["mustache"]).on("change", bs.reload);
-  gulp.watch("src/scripts/*.js", ["scripts"]).on("change", bs.reload);
-});
+function sync() {
+  bs.init({
+    server: "./dist",
+    open: false,
+    notify: false
+  });
+};
 
-//Build Files
-gulp.task("build", ["mustache", "sass", "scripts"]);
+function refresh(done) {
+  bs.reload();
+  done();
+}
 
-//Set up default task
-gulp.task("default", ["watch", "mustache", "sass", "scripts", "browser-sync"]);
+function watch() {
+  gulp.watch("src/styles/**/*.scss", styles)
+  gulp.watch(["src/**/*.mustache", "src/data/*.json"], gulp.series(templates, refresh))
+  gulp.watch("src/scripts/*.js", gulp.series(scripts, refresh))
+};
+
+exports.styles = styles;
+exports.templates = templates;
+exports.scripts = scripts;
+exports.build = gulp.parallel(templates, styles, scripts)
+exports.default = gulp.parallel(watch, templates, styles, scripts, sync)
