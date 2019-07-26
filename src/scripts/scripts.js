40122f09f684641e07b2d01d50d7492b8eea49b8
caseycallow <casey.callow@gmail.com>
2019-07-26 13:06:14 -0700

    create bg image style programmatically to assure animation runs at every page load

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,10 +1,21 @@
+"use strict";
+
+function triggerBackgroundAnimation() {
+  var url = "images/animated-bg.svg?" + Math.random();
+  var css = "body:before { background: url(" + url + ") }";
+  var style = document.createElement("style");
+  style.appendChild(document.createTextNode(css));
+  document.head.appendChild(style);
+}
+
 function updateYear() {
   var date = new Date();
-  year = date.getYear();
+  var year = date.getYear();
   if (year < 1000) year += 1900;
   document.getElementById("copyright-year").innerHTML = year;
 }
 
 window.onload = function() {
+  triggerBackgroundAnimation();
   updateYear();
 };
